73273f17f0cd3b03b77920ade1c0ef6c8aed081c
IONgod1 <lopotencoion1qgmail.com>
2024-11-22 18:53:54 +0200

    Add readme.md

---
+++ sprint9/tema1.js
@@ -0,0 +1,22 @@
+// Scrieți o funcție care creează un obiect pentru a păstra informații despre rețeta ta preferată
+
+function getReceipe(title, mumberOfPortions, incredients){
+    const reteta = {
+        titlu : title,
+        portii : mumberOfPortions,
+        incrediente : incredients,
+    };
+
+    return reteta;
+}
+
+const showReceipe = (reteta) => {
+    console.log(`Reteta noastra preferata de astazi este: ${reteta.titlu}, pentru ${reteta.portii} portii, cu urmatoarele incrediente: ${reteta.incrediente}.`);
+
+}
+
+const reteta1 = getReceipe("Pizza", 3, ['sunca', 'cas', 'ketchup']);
+const reteta2 = getReceipe("salata", 5, ['rosii', 'castraveti', 'cascaval', 'salata', 'morcovi']);
+
+showReceipe(reteta1);
+showReceipe(reteta2);
